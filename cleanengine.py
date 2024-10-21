aa547589ee51d7d286f506014f274e7408771364
wallga21 <95170212+wallga21@users.noreply.github.com>
2024-10-21 20:21:14 +0900

    Update cleanengine.py

--- cleanengine.py
+++ cleanengine.py
@@ -5,7 +5,7 @@ model = GLiNER.from_pretrained("urchade/gliner_mediumv2.1")
 from tensorflow.keras.models import load_model
 import pickle
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-model = load_model('goodbad_model.h5')
+model2 = load_model('goodbad_model.h5')
 with open('tokenizer.pkl', 'rb') as f:
     tokenizer = pickle.load(f)
 
@@ -13,7 +13,7 @@ def goodbad(new_text):
     max_sequence_length = 100
     sequence = tokenizer.texts_to_sequences([new_text])
     padded_sequence = pad_sequences(sequence, maxlen=max_sequence_length)
-    return model.predict(padded_sequence)<=0.85
+    return model2.predict(padded_sequence)<=0.85
 
 # Mapping from Compatibility Jamo to Hangul Jamo (Choseong)
 compat_to_standard_initials = {
